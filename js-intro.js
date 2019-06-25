4157cd0bf3e7e5576efc56907cced3656edd4cc5
Ubuntu <ubuntu@ip-172-31-3-176.us-east-2.compute.internal>
2019-06-25 01:03:59 +0000

    updatedassessment

--- js-intro.js
+++ js-intro.js
@@ -2,37 +2,33 @@
 
 // Consider this variable:
 
-//var mantra = "B"
+    var mantra = "Be the Dev"
+
+
 
 // 1. Write the code that returns 'B' from mantra.
-   // console.log(mantra)
+    console.log(mantra[0])
+    
+   
 
 
 // 2. Write the code that determines if there is a 'x' in mantra.
 
-//var mantra = "Be the Dev"
-//function mantra (){
-  // if(mantra.includes("")){
-  //      return "true"
-//    } else {
-//        return "false"
-//    }
-//}
-  //  console.log(mantra.includes("x"))
+var mantra = "Be the Dev"
+
+
+    console.log(mantra.includes("x"))
     
 
 // 3. Write the code that determines if there is a 'v' in mantra.
+
 //var mantra = "Be the Dev"
-//function mantra (){
- //   if(mantra.includes("")){
-  //      return "true"
- //   } else {
- //       return "false"
- //   }
-//}
- //   console.log(mantra.includes("v"))
+
+
+  console.log(mantra.includes("v"))
 
 // Stretch: Write the code that returns the position of 'v' in mantra.
+
 
 //var mantra = "Be the Dev"
 //function mantra (){
@@ -44,36 +40,42 @@
 
 // Consider the following statement:
 
-//var codingIsEasy
-//var imBored
+// var codingIsEasy = true
+// var imBored = true 
 
-//if (codingIsEasy && imBored) {
-//    console.log("I need more!")
-//} else {
-   // console.log("Steady as she goes")
-//}
+// if (codingIsEasy && imBored) {
+//     console.log("I need more!")
+// } else {
+//     console.log("Steady as she goes")
+// }
 
 // 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
 
 // Consider the following statement:
 
-//var imLost
-//var imFrustrated
-//var imGreat
+// var imLost = false
+// var imFrustrated = false
 
-//if (imLost || imFrustrated) {
-  //  console.log('Break time!')
-//} else {
-  //  console.log('Keep coding!')
-//}
+
+// if (imLost || imFrustrated) {
+//     console.log('Break time!')
+// } else {
+//     console.log('Keep coding!')
+// }
+
 
 // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
 
 // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
-//for(let i = 0 ; i < mantra.length ; i++)
-//console.log(mantra[i])
+
+    var newLetters = mantra.split("")
+    for (let i= 0; i<newLetters.length; i++){ 
+        console.log(newLetters[i])
+    }
 
 // Stretch: Using a while loop.
+
+
 
 
 // Super Stretch: Using forEach().
@@ -93,14 +95,18 @@
 
 // Consider the following variable:
 
-//var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
+var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
 // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
-//onlyCats = (arr) => {
-   // var newArr = [0]
-   // for(let i = 0; i < arr.length; i++){
-   //     newArr.push 
-  //  }
-  //  return newArr
-//}
-//console.log(onlyCats(toonimals))
+
+const onlyCats = (arr) => {
+    var newArr = []
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].animal === 'cat'){
+        newArr.push(arr[i]) 
+        }
+    }
+    return newArr
+}
+
+console.log(onlyCats(toonimals))
